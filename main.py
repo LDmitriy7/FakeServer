8804920f3aea136b7d777213a28908235f79c623
levch <levchenko.d.a1998@gmail.com>
2021-09-07 02:18:15 +0300

    initial

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
-import ssl
-
-from aiohttp import web
+import mongoengine as me
+import uvicorn
+from fastapi import FastAPI, HTTPException, responses, Request, status
+from pydantic import BaseModel
 
 import config
 
-app = web.Application()
+me.connect('url_shortener')
+app = FastAPI()
 
 
-async def index(_request: web.Request):
-    return web.Response(text='Hello, world')
+class RedirectRule(BaseModel):
+    key: str
+    url: str
 
 
-app.router.add_get('/', index)
+class RedirectRuleDoc(me.Document):
+    key: str = me.StringField(unique=True)
+    url: str = me.StringField()
 
-ssl_context = ssl.SSLContext()
-ssl_context.load_cert_chain(config.CERT_FILE, config.KEY_FILE)
 
-web.run_app(app, host=config.HOST, port=config.PORT, ssl_context=ssl_context)
+@app.get('/redirect/{key}')
+def do_redirect(key: str):
+    redirect_rule_doc: RedirectRuleDoc = RedirectRuleDoc.objects(key=key).first()
+    if redirect_rule_doc is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+    return responses.RedirectResponse(redirect_rule_doc.url)
+
+
+@app.post('/redirect')
+def add_redirect_rule(request: Request, redirect_rule: RedirectRule, token: str):
+    if token != config.TOKEN:
+        raise HTTPException(401)
+
+    redirect_rule_doc: RedirectRuleDoc = RedirectRuleDoc.objects(key=redirect_rule.key).first()
+
+    if redirect_rule_doc:
+        redirect_rule_doc.url = redirect_rule.url
+        redirect_rule_doc.save()
+    else:
+        RedirectRuleDoc(**redirect_rule.dict()).save()
+
+    return {'short_url': request.url_for('do_redirect', key=redirect_rule.key)}
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'main:app',
+        host=config.HOST,
+        port=config.PORT,
+        ssl_certfile=config.CERT_FILE,
+        ssl_keyfile=config.KEY_FILE,
+    )
