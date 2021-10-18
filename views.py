9baeba497b11c1e13b82bc9bcd3b29225df429dc
levch <levchenko.d.a1998@gmail.com>
2021-10-18 04:21:26 +0300

    .

--- views.py
+++ views.py
@@ -1,33 +1,36 @@
 from typing import Optional
 
-from fastapi import HTTPException, responses, status, Request, Header
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import HTTPException, responses, status, Response, Request, Header
+# from fastapi.middleware.cors import CORSMiddleware
 
 import config
 from loader import app
 from models import documents, models
 
-origins = [
-    "https://my-bots.ru",
-]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# origins = [
+#     "https://my-bots.ru",
+# ]
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 @app.get('/go/{rule_key}')
-def go_rule_by_key(rule_key: str):
+def go_rule_by_key(response: Response, rule_key: str):
     go_rule_doc: documents.GoRule = documents.GoRule.objects(key=rule_key).first()
 
     if go_rule_doc is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND)
 
-    return responses.RedirectResponse(go_rule_doc.url)
+    # response.headers['Referer-policy'] = 'origin'
+
+    return responses.RedirectResponse(go_rule_doc.url, headers={'Referer-policy': 'origin'})
 
 
 @app.post('/go')
